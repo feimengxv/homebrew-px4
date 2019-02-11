7e3d8ef8933cd1bcef8e5f85766957e312590752
feimengxv <46960088+feimengxv@users.noreply.github.com>
2019-02-11 20:51:03 +0800

    Update gcc-arm-none-eabi.rb

--- Formula/gcc-arm-none-eabi.rb
+++ Formula/gcc-arm-none-eabi.rb
@@ -3,8 +3,8 @@ require "formula"
 class GccArmNoneEabi < Formula
   desc "GNU Embedded Toolchain for ARM"
   homepage "https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads"
-  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-mac.tar.bz2"
-  version "20180627"
+  url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-8-2018-q4-major-mac.tar.bz"
+  version "20181220"
   sha256 "c1c4af5226d52bd1b688cf1bd78f60eeea53b19fb337ef1df4380d752ba88759"
 
   def install
